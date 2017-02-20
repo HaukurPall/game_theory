0d43b655b5dcaa3c848e9a4f7fb4bcf4c65b3ed1
HaukurPall <haukurpalljonsson@gmail.com>
2017-02-20 22:24:20 +0100

    homework2 done

--- hw2/homework2.py
+++ hw2/homework2.py
@@ -1,8 +1,9 @@
 def a_1():
     import math
-    for n in range(1, 201):
+    for n in range(0, 201):
         sum = 0
         for k in range(0, 200-n):
             sum += math.sqrt(k)
-        p = 10*n + sum
+        p = -(10*n + sum)
         print("n={}: {}".format(n,p))
+a_1()
