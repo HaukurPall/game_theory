d82a8c322aba6680bf8e8479ab00dbf29c635da9
HaukurPall <haukurpalljonsson@gmail.com>
2017-05-02 16:51:19 +0200

    report done

--- comp_social_choice/hw3/report.py
+++ comp_social_choice/hw3/report.py
@@ -1,5 +1,6 @@
 from comp_social_choice.hw3.manipulability import Preference, Profile, BordaRule, PluralityRule, CopelandRule
 import matplotlib.pyplot as plt
+import time
 
 
 def plot_line_graph(x, y, y_name, x_name):
@@ -30,17 +31,17 @@ def create_graphs(data_file, graph_name):
         x,
         y_plurality,
         'o',
-        label = 'plurality'
+        label='plurality'
     )
     trace1 = ax.plot(
         x,
         y_borda,
         'o',
-        label = 'borda/copeland'
+        label='borda/copeland'
     )
 
     ax.legend(loc='upper center', shadow=True)
-    ax.set_ylim([0,100])
+    ax.set_ylim([0, 100])
     plt.savefig(graph_name)
     plt.clf()
 
@@ -52,8 +53,9 @@ def compute_averages(data_file):
     for index, voter_count in enumerate(x):
         plurality_sum += y_plurality[index]
         borda_sum += y_borda[index]
-    print("plurality: " + str(plurality_sum/len(x)))
-    print("borda: " + str(borda_sum/len(x)))
+    print("plurality: " + str(plurality_sum / len(x)))
+    print("borda: " + str(borda_sum / len(x)))
+
 
 def extract_counts(data_file):
     with open(data_file) as data_file:
@@ -86,23 +88,33 @@ def extract_counts(data_file):
     return x, y_borda, y_plurality, y_copeland
 
 
-def generate_data(file_name):
-    number_of_candidates = 3
-    number_of_profiles = 100
-    number_of_voters = 10
-    limit = 1000
+def write_aggregated_data_to_file(output_file, input_file):
+    x, y_borda, y_plurality, y_copeland = extract_counts(input_file)
+    with open(output_file + "_plurality", "a") as data_file:
+        for i in range(0, len(x)):
+            data_file.write(",".join([str(x[i]), str(y_plurality[i])]) + "\n")
+    with open(output_file + "_borda", "a") as data_file:
+        for i in range(0, len(x)):
+            data_file.write(",".join([str(x[i]), str(y_borda[i])]) + "\n")
+
+
+def generate_data(file_name, number_of_candidates, number_of_profiles, start_voters, end_voters, increment_voters):
     plurality_rule = PluralityRule()
     borda_rule = BordaRule()
     copeland_rule = CopelandRule()
+    number_of_voters = start_voters
     with open(file_name, "a") as file:
         file.write("rule,voters,candidates,manipulable\n")
-        while number_of_voters <= limit:
+        while number_of_voters <= end_voters:
             profiles = generate_profiles(number_of_voters, number_of_candidates, number_of_profiles)
             for profile in profiles:
-                write_to_file(file, number_of_candidates, number_of_voters, plurality_rule, profile.is_manipulable(plurality_rule))
-                write_to_file(file, number_of_candidates, number_of_voters, borda_rule, profile.is_manipulable(borda_rule))
-                write_to_file(file, number_of_candidates, number_of_voters, copeland_rule, profile.is_manipulable(copeland_rule))
-            number_of_voters += 10
+                write_to_file(file, number_of_candidates, number_of_voters, plurality_rule,
+                              profile.is_manipulable(plurality_rule))
+                write_to_file(file, number_of_candidates, number_of_voters, borda_rule,
+                              profile.is_manipulable(borda_rule))
+                write_to_file(file, number_of_candidates, number_of_voters, copeland_rule,
+                              profile.is_manipulable(copeland_rule))
+            number_of_voters += increment_voters
 
 
 def write_to_file(file, number_of_candidates, number_of_voters, rule, manipulable):
@@ -113,9 +125,14 @@ def write_to_file(file, number_of_candidates, number_of_voters, rule, manipulabl
 
 
 def main():
-    compute_averages("6_candidates_csv.csv")
-    #create_graphs("3_candidates_csv.csv", "3 candidates")
-    #generate_data("3_candidates_csv.csv")
+    t0 = time.time()
+    generate_data("10_candidates_csv.csv", 10, 100, 10, 1000, 10)
+    create_graphs("10_candidates_csv.csv", "10 candidates")
+    compute_averages("10_candidates_csv.csv")
+    #write_aggregated_data_to_file("6_candidates_aggregated", "6_candidates_csv.csv")
+    t1 = time.time()
+    total = t1-t0
+    print("Total time taken: " + str(total))
 
 if __name__ == "__main__":
     main()
